98c3f6cac26cbf25a3bb926e4e22b1ef363db83b
phj9020 <phj9020@hotmail.com>
2022-07-10 15:02:44 +0900

    initial

---
+++ src/style.d.ts
@@ -0,0 +1,16 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    color: string;
+    backgroundColor: string;
+    accentColor: string;
+}
+export interface DarkTheme {
+    color: string;
+    backgroundColor: string;
+    accentColor: string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DarkTheme, DefaultTheme } from 'styled-components';
+
+export const myTheme: DefaultTheme = {
+    color: "black",
+    backgroundColor: "white",
+    accentColor: "purple"
+};
+
+export const darkTheme: DarkTheme = {
+    color: "white",
+    backgroundColor: "black",
+    accentColor: "green"
+}
