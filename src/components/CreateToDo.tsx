035c70ea01303a379c58dc887e64e260708dce92
phj9020 <phj9020@hotmail.com>
2022-07-17 14:18:53 +0900

    [State Immutability] when state changed,data has to be immutable, so return with new array

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -26,7 +26,7 @@ function CreateToDo() {
         <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
         <h1 style={{margin:"20px 0px"}}>할 일을 작성해주세요</h1>
         <input style={{padding:"10px"} }{...register("toDo", { required: "필수 입력값입니다", minLength: {value:4, message:"4자 이상 할 일을 입력해주세요"} })} placeholder="Write a to do" />
-        <span style={{color:"red"}}>{errors?.toDo?.message}</span>
+        <span style={{color:"red", fontSize:"12px", marginTop:"5px"}}>{errors?.toDo?.message}</span>
         <button style={{marginTop:"20px", padding:"10px", cursor: "pointer", backgroundColor:"skyblue", border:"none", color:"white", fontWeight:"bold"}} type="submit">Add</button>
     </form>
     )
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,28 @@
-import { IToDo } from '../atoms';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoAtom } from '../atoms';
 
 function ToDo({text, id, category}: IToDo) {
+    const setToDos = useSetRecoilState(toDoAtom);
+    const onClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        const {currentTarget : {name}} = e;
+        setToDos((oldToDos)=> {
+            // Step1 : find target's index number
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            // Step2 : set newToDo with updated category("Doing", "To_Do", "Done") 
+            const newToDo = {id, text, category: name as any}
+            // Step4 : replace old oldToDo with newToDo
+            const first = oldToDos.slice(0, targetIndex);
+            const back = oldToDos.slice(targetIndex+1);
+            
+            return [...first, newToDo, ...back];
+        })
+    }
     return (
         <li>
             <span>{text}</span> 
-            <button>Done</button>
-            <button>Doing</button>
-            <button>To Do</button>
+            {category !== "Doing" && <button name="Doing" onClick={onClick}>진행중</button>}
+            {category !== "To_Do" && <button name="To_Do" onClick={onClick}>할 일</button>}
+            {category !== "Done" && <button name="Done" onClick={onClick}>완료</button>}
         </li>
     )
 }
