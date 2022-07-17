0cf08ed753ca12bf4d3564ba218930fd9b97b34c
phj9020 <phj9020@hotmail.com>
2022-07-17 14:59:32 +0900

    [Recoil-Selector] selector applied

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
     id: number;
@@ -10,3 +10,17 @@ export const toDoAtom = atom<IToDo[]>({
     key: "toDo",
     default: [],
 });
+
+// Selector는 atom의 output을 변형시키는 도구이다
+// toDoAtom에 변하면 selector도 함께변함
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({get}) => {
+        const toDos = get(toDoAtom);
+        return [
+            toDos.filter(category => category.category === "To_Do"),
+            toDos.filter(category => category.category === "Doing"),
+            toDos.filter(category => category.category === "Done"),
+        ]
+    }, 
+})
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,22 +1,35 @@
 import { useRecoilValue } from "recoil";
-import { toDoAtom } from "../atoms";
+import { toDoAtom, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 
 
 function ToDoList() {
-  // get only stateValue in recoil
-  const toDos = useRecoilValue(toDoAtom);
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+ 
   return (
     <div style={{width: "400px", margin: "50px auto"}}>
         <h1 style={{textAlign: "center", fontSize: "20px", textTransform:"uppercase"}}>HJP Trello</h1>
         <hr />
         <CreateToDo />
-        <ul style={{marginTop: "30px"}}>
-          {toDos.map((todo) => <ToDo key={todo.id} {...todo}  />)}
-        </ul>
+        <div style={{marginTop:"20px"}}>
+          <h2>할 일</h2>
+          <ul style={{marginTop: "30px"}}>
+            {toDo.map((todo) => <ToDo key={todo.id} {...todo}  />)}
+          </ul>
+          <hr/>
+          <h2>진행 중</h2>
+          <ul style={{marginTop: "30px"}}>
+            {doing.map((doing) => <ToDo key={doing.id} {...doing}  />)}
+          </ul>
+          <hr/>
+          <h2>완료</h2>
+          <ul style={{marginTop: "30px"}}>
+            {done.map((done) => <ToDo key={done.id} {...done}  />)}
+          </ul>
+        </div>
+
     </div>
   )
 }
